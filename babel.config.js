f0ef75544f2c7c6e7747801c2a066e6ab21a8f77
joaocarrato <joaocarrato18@gmail.com>
2025-03-25 19:20:33 -0300

    Add Poppins font files and configure ESLint, TypeScript, and Husky pre-commit hooks

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: '.',
+        alias: {
+          '@api': './src/api',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@services': './src/services',
+          '@theme': './src/theme',
+        },
+      },
+    ],
+  ],
 };
